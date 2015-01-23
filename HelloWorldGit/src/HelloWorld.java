277575e6dff2701069d0182ed360eb4195920aaa
Cleberson Pauluci <cleberson.pauluci@ig.com.br>
2015-01-23 09:00:50 -0200

    Criando a classe HelloWorld
    Testando a implementação da classes HelloWorld com Git!
    
    
    Signed-off-by: Cleberson Pauluci <cleberson.pauluci@ig.com.br>

---
+++ HelloWorldGit/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	/**
+	 * Implementando a aplicação
+	 * 
+	 * @param args arguments
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World com Git");
+	}
+	
+}
