1a0a8e3129e3f6fefd6f79910ead0cf80a07695d
Cleberson Pauluci <cleberson.pauluci@ig.com.br>
2015-01-23 10:34:49 -0200

    Testando o commit com o server github
    Criada a segunda classe para testes.
    
    Signed-off-by: Cleberson Pauluci <cleberson.pauluci@ig.com.br>

---
+++ HelloWorldGit/src/SegundaClasse.java
@@ -0,0 +1,16 @@
+
+public class SegundaClasse {
+
+	private String segundaClasseParaTeste;
+
+	public String getSegundaClasseParaTeste() {
+		return segundaClasseParaTeste;
+	}
+
+	public void setSegundaClasseParaTeste(String segundaClasseParaTeste) {
+		this.segundaClasseParaTeste = segundaClasseParaTeste;
+	}
+	
+	
+	
+}
