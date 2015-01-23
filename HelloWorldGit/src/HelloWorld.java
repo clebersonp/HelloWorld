12297374d5fdc7d0274120f0d5eeed2bd57e5cfd
Cleberson Pauluci <cleberson.pauluci@ig.com.br>
2015-01-23 09:19:42 -0200

    Quarto teste
    Conhecendo os botões do Git

--- HelloWorldGit/src/HelloWorld.java
+++ HelloWorldGit/src/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World com Git");
 		System.out.println("Segunda mensagem para teste");
-		System.out.println("Terceira entrada para teste");
+		System.out.println("Terceira entrada para t");
 	}
 	
 }
