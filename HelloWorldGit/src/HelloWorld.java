21753293463ce795e58e7d8daf62148f35e9deef
Cleberson Pauluci <cleberson.pauluci@ig.com.br>
2015-01-23 09:05:53 -0200

    Segunda alteração
    Alteração na classe HelloWorld, adicionando uma nova mensagem.
    
    
    Signed-off-by: Cleberson Pauluci <cleberson.pauluci@ig.com.br>

--- HelloWorldGit/src/HelloWorld.java
+++ HelloWorldGit/src/HelloWorld.java
@@ -4,11 +4,14 @@ public class HelloWorld {
 	/**
 	 * Implementando a aplicação
 	 * 
+	 * Testando o git no Eclipse Luna
+	 * 
 	 * @param args arguments
 	 */
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World com Git");
+		System.out.println("Segunda mensagem para teste");
 	}
 	
 }
